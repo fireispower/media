f6988168aa4c15d9be86b2cebd04b83c89340e2b
Starlord <65814962+fireispower@users.noreply.github.com>
2024-01-08 01:19:18 +0530

    Update index.js

--- index.js
+++ index.js
@@ -82,7 +82,7 @@ let bot = new TelegramBot(token, {
 })
 // Bot Settings
 let botName = 'Nezuko Social Bot';
-let strtImg = '[Hello](https://telegra.ph/file/2a8da5333524a11964b64.jpg)';
+
 app.get('/', async (req, res) => {
   res.send({
     Status: "Active"
