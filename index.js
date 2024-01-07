6e02e70bfc8fcde74bdd1c6675baa72b19d1ca2f
Starlord <65814962+fireispower@users.noreply.github.com>
2024-01-08 01:12:37 +0530

    Update index.js

--- index.js
+++ index.js
@@ -82,7 +82,7 @@ let bot = new TelegramBot(token, {
 })
 // Bot Settings
 let botName = 'Nezuko Social Bot';
-let strtImg = '<img src="https://telegra.ph/file/2a8da5333524a11964b64.jpg" alt="Hello">'
+let strtImg = '<img src="https://telegra.ph/file/2a8da5333524a11964b64.jpg" alt="Hello">';
 app.get('/', async (req, res) => {
   res.send({
     Status: "Active"
@@ -125,7 +125,7 @@ bot.on('photo', async (msg) => {
 
 // start
 bot.onText(/\/start/, async (msg) => {
-  let response = `[Hello](buttonurl:https://telegra.ph/file/2a8da5333524a11964b64.jpg) I am ${botName}
+  let response = `{strtImg} I am ${botName}
 
 Please send a link to the video or post you want to download, the bot only supports social media on the list
 
